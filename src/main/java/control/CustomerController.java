bf4c22a6d2ccde69c807ea36047d6452a4c62a6e
shpaek <seohpp@gmail.com>
2023-11-01 17:19:19 +0900

    [feat] 로그아웃 구현

--- src/main/java/control/CustomerController.java
+++ src/main/java/control/CustomerController.java
@@ -1,14 +1,29 @@
 package control;
 
-import com.my.customer.service.CustomerService;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.my.customer.service.CustomerServiceImpl;
 
-public abstract class CustomerController implements Controller {
+@RestController
+@RequestMapping("/main")
+public abstract class CustomerController {
+	
+	@Autowired
+	protected CustomerServiceImpl impl;
 
-	protected CustomerService service;
 
-	public CustomerController() {
-		service = CustomerServiceImpl.getInstance();
-	} // constructor
+
+	@GetMapping("/logout")
+	public void logout(HttpSession session) {
+			session.removeAttribute("loginedId");
+			session.invalidate();
+	}
+		
+	
 
 } // end class
