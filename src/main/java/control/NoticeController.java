05ddc8246df49b343c7a4375932e0efcc8fffafc
nawonhee <nwh0921@naver.com>
2023-11-06 17:42:27 +0900

    [Fix] editnotice, deletenotice 스프링 변환

--- src/main/java/control/NoticeController.java
+++ src/main/java/control/NoticeController.java
@@ -2,6 +2,7 @@ package control;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.time.Instant;
@@ -21,10 +22,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.my.exception.FindException;
+import com.my.exception.RemoveException;
 import com.my.notice.dto.NoticeDTO;
 import com.my.notice.service.NoticeService;
 import com.my.notice.service.NoticeServiceImpl;
-import com.my.product.dto.Product;
 import com.my.team.service.TeamService;
 import com.my.util.Attach;
 import com.my.util.PageGroup;
@@ -210,22 +211,100 @@ public abstract class NoticeController {
 			String existFileName = f.getName();
 			if(existFileName.startsWith(fileName)) {
 		
-				HttpStatus status = HttpStatus.OK;
-				HttpHeaders headers = new HttpHeaders();
-				headers.add(HttpHeaders.CONTENT_DISPOSITION,
-						    "attachment;filename=" + URLEncoder.encode(existFileName, "UTF-8"));
-				
-				System.out.println("in download file: " + f + ", file size:" + f.length());
-				String contentType = Files.probeContentType(f.toPath());//파일의 형식		
-				headers.add(HttpHeaders.CONTENT_TYPE, contentType); //응답형식		
-				headers.add(HttpHeaders.CONTENT_LENGTH, ""+f.length());//응답길이
-				
-				byte[]bArr = FileCopyUtils.copyToByteArray(f);
-				entity = new ResponseEntity<>(bArr, headers, status);//응답상태코드
-				return entity;
+				try{
+					HttpStatus status = HttpStatus.OK;
+					HttpHeaders headers = new HttpHeaders();
+					headers.add(HttpHeaders.CONTENT_DISPOSITION,
+							    "attachment;filename=" + URLEncoder.encode(existFileName, "UTF-8"));
+					
+					System.out.println("in download file: " + f + ", file size:" + f.length());
+					String contentType = Files.probeContentType(f.toPath());//파일의 형식		
+					headers.add(HttpHeaders.CONTENT_TYPE, contentType); //응답형식		
+					headers.add(HttpHeaders.CONTENT_LENGTH, ""+f.length());//응답길이
+					
+					byte[]bArr = FileCopyUtils.copyToByteArray(f);
+					entity = new ResponseEntity<>(bArr, headers, status);//응답상태코드
+					return entity;
+				}catch(Exception e) {
+					e.printStackTrace();
+					return null;
+				}
 			}
 		}
 
 		return entity;
+	}
+	
+	@GetMapping("/editnotice")
+	public Map<String, Object> editnotice(Integer teamNo, NoticeDTO notice, Integer status, MultipartFile f1) {
+		Map<String, Object> map = new HashMap<>();
+		Integer mainStatus = 0;
+		Integer mainChk = 0;
+		String attachesDir = "C:\\KOSA202307\\attaches";
+		File dir = new File(attachesDir);
+		try{
+			if(status != null) {
+				mainStatus = 1;
+				NoticeDTO mainNotice = noticeService.findMainNotice(teamNo);
+				if(mainNotice!=null) {
+					if(mainNotice.getNoticeNo()==notice.getNoticeNo()) {
+						map.put("mainmsg", "이미 메인공지로 등록된 게시글입니다");
+						mainStatus=0;
+					}else {
+						map.put("mainmsg", "이미 메인공지가 존재합니다\n기존 메인공지를 내린 후 시도해주세요");
+						mainStatus=0;
+					}
+				}else {
+					mainChk = 1;
+				}
+			}else {
+				mainChk=1;
+			}
+			
+			notice.setMainStatus(mainStatus);
+			noticeService.modifyNotice(teamNo, notice);
+			
+			String findName = teamNo+"_"+notice.getNoticeNo()+"_notice_";
+			
+			if(f1 != null && f1.getSize() > 0) {				
+				String originFileName = f1.getOriginalFilename();
+				for(File file : dir.listFiles()) {
+					String existFileName = file.getName();
+					if(existFileName.startsWith(findName)) {
+						file.delete();
+					}
+				}
+				String targetFileName = teamNo+"_"+notice.getNoticeNo()+"_notice_"+ originFileName;
+				File targetFile = new File(attachesDir, targetFileName);
+				FileCopyUtils.copy(f1.getBytes(), targetFile);
+			}
+			
+			map.put("mainstatus", mainChk);
+			map.put("status", 1);
+			map.put("msg", "게시글이 수정되었습니다");
+			return map;
+		}catch (Exception e) {
+			e.printStackTrace();
+			map.put("status", 0);
+			map.put("msg", e.getMessage());
+			return map;
+		}
+	}
+	
+	@GetMapping("/deletenotice")
+	public Map<String, Object> deleteNotice(Integer teamNo, Integer noticeNo) {
+		Map<String, Object> map = new HashMap<>();
+
+		try {
+			noticeService.removeNotice(teamNo, noticeNo);
+			map.put("status", 1);
+			map.put("msg", "삭제되었습니다");
+		} catch (RemoveException e) {
+			e.printStackTrace();
+			map.put("status", 0);
+			map.put("msg", e.getMessage());
+		}
+
+		return map;
 	}
 }
